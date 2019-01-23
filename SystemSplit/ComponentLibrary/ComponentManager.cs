5de73d01b7746725356ea672c1d0d52d9b6b3c1e
Christian Nytra <ch.nytra.13@gmail.com>
2019-01-23 11:12:52 +0100

    managermethodes added

--- SystemSplit/ComponentLibrary/ComponentManager.cs
+++ SystemSplit/ComponentLibrary/ComponentManager.cs
@@ -8,6 +8,39 @@ namespace ComponentLibrary
 {
     public class ComponentManager
     {
+        public void RegisterHeavyHardware(string name, int capacity, int memory)
+        {
 
+        }
+
+        public void RegisterPowerHardware(string name, int capacity, int memory)
+        {
+
+        }
+
+        public void RegisterExpressSoftware(string name, int capacityConsumption, int memoryConsumption)
+        {
+
+        }
+
+        public void RegisterLightSoftware(string name, int capacityConsumption, int memoryConsumption)
+        {
+
+        }
+
+        public void ReleaseSoftwareComponent()
+        {
+
+        }
+
+        public void Analyze()
+        {
+
+        }
+
+        public void SystemSplit()
+        {
+
+        }
     }
 }
